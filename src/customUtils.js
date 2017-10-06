62a9b68823e0795e749d525e2104e202f6ba837c
alexhans1 <alexander.hans.mail@gmail.com>
2017-10-07 00:13:25 +0200

    Updates

--- src/customUtils.js
+++
@@ -1,9 +0,0 @@
-export function makeid(n)  {
-  let text = "";
-  let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
-  for (let i = 0; i < n; i++)
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
-
-  return text;
-}
