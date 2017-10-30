f385e9efbb012e9069db48a2745019856adb1223
alexhans1 <alexander.hans.mail@gmail.com>
2017-10-30 20:50:05 +0100

    refactored navbar using reactstrap and react router Links

--- frontend/src/Components/layout/Navbar/Navbar.js
+++ frontend/src/Components/layout/Navbar/Navbar.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
 import InfoModal from './InfoModal'
+import { Link } from 'react-router-dom';
 import '../../../stylesheets/Navbar.css';
 
-class Navbar extends Component {
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+
+class myNavbar extends Component {
 
   constructor() {
     super();
+
+    this.toggleNavbar = this.toggleNavbar.bind(this);
     this.state = {
       showModal: false,
+      isOpen: false,
     };
+  }
+
+  toggleNavbar() {
+    this.setState({
+      isOpen: !this.state.isOpen,
+    });
   }
 
   toggleModal() {
@@ -20,26 +32,33 @@ class Navbar extends Component {
   render() {
     return (
       <div className="container-fluid">
-        <div id={"navbar-row"} className="row">
-          <div className="col-2">
+        <Navbar color="faded" light expand="md">
+          <Link to={"/"} className={"navbar-brand"}>
             <img className={""} src="/images/logo.png" width="120" height="120" alt="" />
-          </div>
-          <div className="col-10">
-            <nav className="navbar">
-            <span onClick={this.toggleModal.bind(this)} className={"ml-auto p-2 pointer"}>
-              <i className="fa fa-question-circle-o fa-lg" aria-hidden="true" />
-            </span>
-            </nav>
-          </div>
+          </Link>
+          <NavbarToggler onClick={this.toggleNavbar} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav navbar>
+              <NavItem>
+                <Link to={"/"} className={"nav-link"}>
+                  Home
+                </Link>
+              </NavItem>
+              <NavItem className={"infoModalButton"}>
+                <NavLink onClick={this.toggleModal.bind(this)} className={"p-2 pointer"}>
+                  <i className="fa fa-question-circle-o fa-lg" aria-hidden="true" />
+                </NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
 
-          <InfoModal showModal={this.state.showModal} toggle={this.toggleModal.bind(this)} />
-        </div>
+        <InfoModal showModal={this.state.showModal} toggle={this.toggleModal.bind(this)} />
       </div>
-
     )
   }
 }
 
-export default Navbar;
+export default myNavbar;
 
 
