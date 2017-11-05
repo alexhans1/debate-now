6f52c29b78351f5a043e65a17383d119fc74bad4
alexhans1 <alexander.hans.mail@gmail.com>
2017-11-05 12:19:37 +0100

    removed password for deleting event when already in localstorage

--- src/Components/Main/DeleteEventModal.js
+++ src/Components/Main/DeleteEventModal.js
@@ -44,26 +44,48 @@ class DeleteEventModal extends Component {
         Delete
       </Button>;
 
+    let modal =
+      <Modal isOpen={this.props.showModal} toggle={this.toggle}>
+        <ModalHeader className={"black"} toggle={this.toggle}>Type the password for the event to delete it.</ModalHeader>
+        <ModalBody className={"black"}>
+          <Form onSubmit={this.handleSubmit.bind(this)}>
+
+            <FormGroup row>
+              <Label for="eventPassword" sm={3}>Password</Label>
+              <Col sm={9}>
+                <Input type="text" onChange={this.handlePasswordChange.bind(this)}
+                       name="event password" id="eventPassword"
+                       placeholder="Password for editing this event" required />
+              </Col>
+            </FormGroup>
+
+            {deleteButton}
+          </Form>
+        </ModalBody>
+      </Modal>;
+
+    if (this.props.event) {
+      if (JSON.parse(localStorage.getItem('canEdit')).includes(this.props.event.id)) {
+        modal =
+          <Modal isOpen={this.props.showModal} toggle={this.toggle}>
+            <ModalHeader className={"black"} toggle={this.toggle}>Are you sure you want to delete this event?</ModalHeader>
+            <ModalBody className={"black"}>
+              <Form onSubmit={this.handleSubmit.bind(this)}>
+
+                <p>{this.props.event.institution} - {this.props.event.type}</p>
+
+                <Button color={"info"} className={"pull-right"}>
+                  Delete
+                </Button>
+              </Form>
+            </ModalBody>
+          </Modal>;
+      }
+    }
+
     return (
       <div>
-        <Modal isOpen={this.props.showModal} toggle={this.toggle}>
-          <ModalHeader className={"black"} toggle={this.toggle}>Type the password for the event to delete it.</ModalHeader>
-          <ModalBody className={"black"}>
-            <Form onSubmit={this.handleSubmit.bind(this)}>
-
-              <FormGroup row>
-                <Label for="eventPassword" sm={3}>Password</Label>
-                <Col sm={9}>
-                  <Input type="text" onChange={this.handlePasswordChange.bind(this)}
-                         name="event password" id="eventPassword"
-                         placeholder="Password for editing this event" required />
-                </Col>
-              </FormGroup>
-
-              {deleteButton}
-            </Form>
-          </ModalBody>
-        </Modal>
+        {modal}
       </div>
     );
   }
